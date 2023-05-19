b70dc922b9e9e2bd4eada86851c37fbf9e4f5150
Mikaela-D <G00401392@atu.ie>
2023-05-19 20:04:50 +0100

    Added a try catch

--- src/main/java/ie/atu/StandardTestConnection.java
+++ src/main/java/ie/atu/StandardTestConnection.java
@@ -1,11 +1,22 @@
 package ie.atu;
 
 import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 
 public class StandardTestConnection {
 
     public static void main(String[] args) {
         Connection connection = null;
-
+        try
+        {
+            // Load the driver class
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            // Create a connection to the database
+            connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/project", "root", "root");
+            System.out.println("Connection made to connection pool");
+        } catch (ClassNotFoundException | SQLException e) {
+            e.printStackTrace();
+        }
     }
 }
