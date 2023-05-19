07044cf0db93151570ce83824c5a4329bc556cc8
Mikaela-D <G00401392@atu.ie>
2023-05-19 20:02:09 +0100

    Added class StandardTestConnection

---
+++ src/main/java/ie/atu/StandardTestConnection.java
@@ -0,0 +1,11 @@
+package ie.atu;
+
+import java.sql.Connection;
+
+public class StandardTestConnection {
+
+    public static void main(String[] args) {
+        Connection connection = null;
+
+    }
+}
